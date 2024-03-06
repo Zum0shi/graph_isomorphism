92331f2ba0333c0f3697c23eb0dc06b3b56d4961
Zum0shi <sberloyarden@gmail.com>
2024-03-06 20:49:35 +0100

    new implementation

--- colorref.py
+++ colorref.py
@@ -1,4 +1,5 @@
 from graph import *
+from collections import defaultdict
 from graph_io import *
 from line_profiler_pycharm import profile
 
@@ -14,30 +15,56 @@ def basic_colorref(path):
 
     # iterate over every graph
     for i in range(len(graph_dict)):
+        print("STARTING GRAPH " + str(i) + " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         iteration = 0
         vertex_dict = graph_dict[i]
-        current = {}
-        previous = {}
-        # populate 'current' with key = neighbors , value = vertex label
+        have_same_neighborhood = defaultdict(set)
+        new_color_classes = defaultdict(set)
+        old_color_classes = {}
+
+        # populate 'new_color_classes' with key = color , value = set(vertex labels)
         for v in range(len(vertex_dict) - 1):
-            current[glist[i].vertices[v].neighbours] = v
+            new_color_classes[vertex_dict[v]].add(v)
 
         # start refining process
-        while current != previous:
+        while old_color_classes != new_color_classes:
             # update dictionaries and iteration counter
             iteration += 1
-            previous = current.copy()
-            current.clear()
-            #DO SOMETHING WITH PREVIOUS
-            # iterate over every vertex
+            old_color_classes = new_color_classes.copy()
+            new_color_classes.clear()
+
+            # iterate over all color classes
+            for c in old_color_classes:
+                have_same_neighborhood.clear()
+
+                # create a dictionary of neighborhoods of the vertices in said color class
+                for vtx in old_color_classes[c]:
+                    have_same_neighborhood[
+                        tuple(get_neighbor_colors(glist[i].vertices[vtx].neighbours, vertex_dict))].add(vtx)
+
+                # iterate over all neighborhoods and change colors of all except the first
+                temp = list(have_same_neighborhood.keys())[1:]
+                for n in temp:
+                    vertex_dict["last_color"] += 1
+                    for ve in have_same_neighborhood[n]:
+                        vertex_dict[ve] = vertex_dict["last_color"]
+
+            # updated color class
             for v in range(len(vertex_dict) - 1):
-                print(v, vertex_dict[v], glist[i].vertices[v].neighbours[0].label)
-    # print(i)
-    # print(graph_dict[i])
+                new_color_classes[vertex_dict[v]].add(v)
+
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ END OF ITERATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+        # printing of some results for debugging
+        print("number of iterations: " + str(iteration))
+        result = []
+        for h in old_color_classes:
+            result.append(len(old_color_classes[h]))
+        result.sort()
+        print(old_color_classes)
+        print("list of partition: " + str(result))
 
 
-# create a dictionary with key = graph index , value vertex dictionary with key = vertex index , value = color
-# sets all vertices to uniform color = 1
 def degree_coloring(glist):
     graph_dict = {}
     for g in range(len(glist)):
@@ -45,9 +72,18 @@ def degree_coloring(glist):
         for v in range(len(glist[g].vertices)):
             vertex_dict[v] = glist[g].vertices[v].degree
         # creates an entry in the dictionary with a variable that stores the highest used color
-        vertex_dict["highest_color"] = max(vertex_dict.values())
+        vertex_dict["last_color"] = max(vertex_dict.values())
         graph_dict[g] = vertex_dict
     return graph_dict
 
 
-basic_colorref("./SampleGraphsBasicColorRefinement/colorref_smallexample_4_16.grl")
+# takes a list of neighbors and a vertex_dict and returns a sorted list of their colors
+def get_neighbor_colors(vlist, vertex_dict):
+    result = []
+    for v in vlist:
+        result.append(vertex_dict[v.label])
+    result.sort()
+    return result
+
+
+basic_colorref("./SampleGraphsBasicColorRefinement/cref9vert3comp_10_27.grl")
